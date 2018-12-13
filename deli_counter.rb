f517a31aadb6485503cd3860e74210da357d19e5
Mohammad Ali <ali.mohammed200@gmail.com>
2018-12-13 01:08:24 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,11 @@
 # Write your code here.
+def line(arr)
+  str = "The line is currently: "
+  if arr.length == 0
+    puts "there is nobody in line"
+    return
+  else
+    arr.each.with_index {|elem, i| str += "#{i + 1}. #{elem}"}
+  end
+  puts str
+end
